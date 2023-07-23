0ea34a8b84796b97512c90a0b66a9481e9f221e9
Bryce Hill <bryh311@gmail.com>
2023-07-23 15:38:30 -0400

    routing bug fixes

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const cors = require('cors')
 const app = express()
 const db = require('./db/database.js')
 const users = require('./routes/api/users.js')
-const subgroups = require('./routes/api/subgroups')
+const subgroups = require('./routes/api/subgroups.js')
 const posts = require('./routes/api/posts.js')
-const comments = require('./routers/api/comments')
+const comments = require('./routes/api/comments.js')
 
 const port = process.env.port || 8080
 
 app.use(cors({origin: true, credentials: true}))
+app.use(express.json({extended: false}))
 
 app.use('/api/users/', users)
 app.use('/api/subgroups/', subgroups)
